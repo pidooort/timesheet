d0a0f97962b4a75d7e0a5247d50fb2ff94cc9a01
avemariaa <qbsvergara@tip.edu.ph>
2024-03-12 14:08:22 +0800

    functional timesheet (with edit update button)

--- src/components/TimesheetManager.jsx
+++ src/components/TimesheetManager.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-import { TextField, Button, Container, Grid, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { TextField, Button, Container, Grid, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 function TimesheetManager() {
   const [employeeInfo, setEmployeeInfo] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: ''
+    name: '',
+    department: '',
+    time: '',
+    date: ''
   });
 
   const [timesheetEntries, setTimesheetEntries] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -22,10 +23,30 @@ function TimesheetManager() {
   const handleAddEntry = () => {
     setTimesheetEntries(prevEntries => [...prevEntries, { ...employeeInfo }]);
     setEmployeeInfo({
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: ''
+      name: '',
+      department: '',
+      time: '',
+      date: ''
+    });
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setEmployeeInfo(timesheetEntries[index]);
+  };
+
+  const handleUpdate = () => {
+    setTimesheetEntries(prevEntries => {
+      const updatedEntries = [...prevEntries];
+      updatedEntries[editIndex] = { ...employeeInfo };
+      return updatedEntries;
+    });
+    setEditIndex(-1);
+    setEmployeeInfo({
+      name: '',
+      department: '',
+      time: '',
+      date: ''
     });
   };
 
@@ -34,50 +55,75 @@ function TimesheetManager() {
       <Typography variant="h4" align="center" gutterBottom>
         Timesheet Manager
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            label="First Name"
-            name="firstName"
-            value={employeeInfo.firstName}
-            onChange={handleChange}
-          />
+      <form>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Name"
+              name="name"
+              value={employeeInfo.name}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Department</InputLabel>
+              <Select
+                value={employeeInfo.department}
+                onChange={handleChange}
+                name="department"
+              >
+                <MenuItem value="ITSO BLDG 3">ITSO BLDG 3</MenuItem>
+                <MenuItem value="ITSO BLDG 5">ITSO BLDG 5</MenuItem>
+                {/* Add more departments as needed */}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Time"
+              name="time"
+              type="time"
+              value={employeeInfo.time}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Date"
+              name="date"
+              type="date"
+              value={employeeInfo.date}
+              onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            {editIndex === -1 ? (
+              <Button type="button" variant="contained" color="primary" onClick={handleAddEntry}>
+                Add Entry
+              </Button>
+            ) : (
+              <div>
+                <Button type="button" variant="contained" color="primary" onClick={handleUpdate}>
+                  Update
+                </Button>
+                <Button type="button" onClick={() => setEditIndex(-1)}>
+                  Cancel
+                </Button>
+              </div>
+            )}
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            label="Last Name"
-            name="lastName"
-            value={employeeInfo.lastName}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Email"
-            name="email"
-            type="email"
-            value={employeeInfo.email}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Phone Number"
-            name="phoneNumber"
-            value={employeeInfo.phoneNumber}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" onClick={handleAddEntry}>
-            Add Entry
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
       <br />
       <Typography variant="h5" align="center" gutterBottom>
         Timesheet Entries
@@ -86,19 +132,23 @@ function TimesheetManager() {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Phone Number</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Department</TableCell>
+              <TableCell>Time In</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {timesheetEntries.map((entry, index) => (
               <TableRow key={index}>
-                <TableCell>{entry.firstName}</TableCell>
-                <TableCell>{entry.lastName}</TableCell>
-                <TableCell>{entry.email}</TableCell>
-                <TableCell>{entry.phoneNumber}</TableCell>
+                <TableCell>{entry.name}</TableCell>
+                <TableCell>{entry.department}</TableCell>
+                <TableCell>{entry.time}</TableCell>
+                <TableCell>{entry.date}</TableCell>
+                <TableCell>
+                  <Button onClick={() => handleEdit(index)}>Edit</Button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
