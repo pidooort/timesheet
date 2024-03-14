518bf7b39070f5c1ca8eaec9da3aa55f2f109588
avemariaa <qbsvergara@tip.edu.ph>
2024-03-14 13:33:52 +0800

    button css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,8 +10,12 @@ function HomePage() {
         <Container>
       <h2>Welcome to the Timesheet Manager</h2>
       {/* Other content */}
+      <p>wahahaha</p>
       <Link to="/timesheet">
-        <Button variant="outlined" color="primary" size="large">Go to Timesheet Manager</Button>
+      <Button variant="contained" sx={{ backgroundColor: '#007bff', color: '#fff', '&:hover': {
+          backgroundColor: '#ff8a65',
+        },
+      }} >Go to Timesheet Manager</Button>
       </Link>
       </Container>
     </div>
