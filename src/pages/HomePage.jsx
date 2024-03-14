3759eff2ecf4dc396d83e71c8ca9991d859833c8
avemariaa <qbsvergara@tip.edu.ph>
2024-03-14 15:58:57 +0800

    header css added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
         <Container>
       <h2>Welcome to the Timesheet Manager</h2>
       {/* Other content */}
-      <p>wahahaha</p>
+      <p>wahahaha makulay ang buhay</p>
       <Link to="/timesheet">
       <Button variant="contained" sx={{ backgroundColor: '#007bff', color: '#fff', '&:hover': {
           backgroundColor: '#ff8a65',
